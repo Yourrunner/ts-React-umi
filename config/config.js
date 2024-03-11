ba99a3565395035d3f48f66077f7df7742be01e4
Pengsiyuan <1519976880@qq.com>
2024-03-11 14:41:55 +0800

    chore: umi学习记录

---
+++ config/config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from '@umijs/max';
+import routes from './routes';
+
+export default defineConfig({
+  antd: {
+    dark: true,
+  },
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes,
+
+  // 路由信息如果写了就是配置式路由，如果没写就是约定式路由
+  // .umirc.ts 文件和 config.js文件二选一即可，如果项目较为简单使用 .umirc.ts ，稍微复杂，或是说想要又较好的拓展性。那么还是使用config.js文件
+  npmClient: 'npm',
+});
